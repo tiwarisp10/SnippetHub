92b3bb07575c06217d0058c14d46ebc185798397
tiwarisp10 <tiwarisp1810@gmail.com>
2026-07-22 17:13:24 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,4 +8,14 @@ const api = axios.create({
   },
 });
 
+export default api;import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
 export default api;
